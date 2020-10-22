9ff9a94dcbfbb75779790891b548cfdecbaf95e8
KDaijiro <72773862+KDaijiro@users.noreply.github.com>
2020-10-22 15:12:55 +0900

    ギリシャ国旗（改訂）
    先ほどの輪郭を消す方法がわかりました。失礼いたしました。

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -2,7 +2,7 @@
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();// noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
@@ -18,14 +18,12 @@ function setup() {
       rect(0, i * d, width, (i + 1) * d);
 
   }
-  fill(255);
-  rect(0,0,5*d,5*d);
-
   fill(blue);
-  let size = d * 2;
+  let size = d * 5;
   rect(0, 0, size, size);
-  rect(0,3*d,size,size);
-  rect(3*d,0,size,size);
-  rect(3*d,3*d,size,size);
+  
+  fill(255);
+  rect(0,2*d,size,d);
+  rect(2*d,0,d,size);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }

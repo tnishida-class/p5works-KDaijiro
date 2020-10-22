9c7a6a4b01167d20dcd2053b62c294d8ff0526ee
KDaijiro <72773862+KDaijiro@users.noreply.github.com>
2020-10-22 15:31:13 +0900

    フランス国旗

---
+++ section2-q4-３/sketch.js
@@ -0,0 +1,14 @@
+// フランス国旗
+function setup() {
+  createCanvas(270, 180);
+  background(255);
+  noStroke();
+  let x = 180;
+  fill(0,0,255);
+  rect(0,0,89.1,x);
+  fill(255);
+  rect(89.1,0,81,x);
+  fill(255,0,0);
+  rect(170.1,0,99.9,x);
+
+}

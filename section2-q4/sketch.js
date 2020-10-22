a44eb6b99423ab1bb11886b51e11d4c0404d4612
KDaijiro <72773862+KDaijiro@users.noreply.github.com>
2020-10-22 15:02:35 +0900

    ギリシャ国旗
    BLANK２の通りにやってみたところ長方形が重なり合っている線が浮き出てしまい、その消し方がわからなかったため違うやり方で行いました。消し方が知りたいです。

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -8,14 +8,24 @@ function setup() {
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
+    if(i % 2 == 0){
+      fill(0,51,160);
+    }
+    else{
+      fill(255);
+    }
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
+      rect(0, i * d, width, (i + 1) * d);
+
   }
+  fill(255);
+  rect(0,0,5*d,5*d);
 
   fill(blue);
-  let size = d * 5;
+  let size = d * 2;
   rect(0, 0, size, size);
-
-  fill(255);
+  rect(0,3*d,size,size);
+  rect(3*d,0,size,size);
+  rect(3*d,3*d,size,size);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }

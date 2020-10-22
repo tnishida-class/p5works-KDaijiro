0a98960a9151f1d84f4e66ad1428319eee20a420
KDaijiro <72773862+KDaijiro@users.noreply.github.com>
2020-10-22 13:41:12 +0900

    チェッカー
    友達の助けを借りました。「！」の発想を学びました。

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,24 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
+  noFill();
   let size = width / 8;
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+  // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    if(!(i % 2 == j % 2)){
+      fill(128);
+      rect(i*size,j*size,size,size);
+    }
+    if(!(i % 2 == j % 2) && j < 3 ){
+      fill(255,0,0);
+      ellipse((i*size + size / 2),(j*size + size / 2),22);
+    }
+    else if(!(i % 2 == j % 2) && j > 4){
+      fill(0);
+      ellipse((i*size + size / 2),(j*size + size / 2),22);
     }
   }
+ }
 }

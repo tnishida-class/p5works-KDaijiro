e67fe429b88ee809213498801c7f1bfeba7500e5
KDaijiro <72773862+KDaijiro@users.noreply.github.com>
2020-11-20 22:17:26 +0900

    section4q-2課題
    回転の関数を調べたらrotateと出てきたので使おうとしましたが、上手く扱えず出来なかったのでこのやり方でしました。

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -5,12 +5,18 @@ function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 5;
+  vy = 5;
+  angle = 0
 }
 
 function draw(){
-  background(160, 192, 255);
+  background(29, 32, 136);
+  fill(255,255,0);
+  star(x, y, 10, angle);
+  x += vx;
+  y += vy;
+  angle += 0.1;
   // BLANK[2] (hint: 作った star 関数を使います)
 
   // 端の処理パターン (1) 反対側から出てくる
@@ -24,12 +30,18 @@ function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    theta += angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
   }
   endShape(CLOSE);
+}
+
+function mouseClicked(){
+  x = mouseX;
+  y = mouseY;
 }
 
 function windowResized(){
